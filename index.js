00934ec57c7fa8ecd31aee3c2eb1f5a03448eee0
HomeMac <homemac@HomeMacs-MacBook-Pro.local>
2022-01-27 21:21:02 -0800

    changed code

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Your Code Here+
+//window.prompt('Do you head left or right?') 
 let firstAnswer  = window.prompt('Do you head left or right?')
+
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
     if (secondAnswer === 'follow it') {
-        let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
+        let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven. Do you want to stay or spread the word ')
         if(secondAnswer === 'stay') {
             let secondAnswer = window.prompt('You live happily amongs the cats for the rest of your days.')
         } else if (secondAnswer === 'spread the word') {
@@ -13,9 +15,36 @@ Do you follow it, or continue on your path?`)
         }
 
     }
+    else if (secondAnswer === 'continue') {
+        let thirdAnswer = window.prompt ('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take? Ladder or Staircase')
+        if (thirdAnswer == 'Ladder') {
+            window.prompt('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
+        } else if ( thirdAnswer == 'Staircase') {
+            window.prompt('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
+        } 
+
+    }
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
-lead you away from the dragon altogether. Which path do you take?`)
+lead you away from the dragon altogether. Which path do you take, Past or Away ?`)
+    if (secondAnswer === 'Past'){
+        let thirdAnswer = window.prompt ('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:')
+        if (thirdAnswer === 'stay') {
+            window.prompt('You and the dragon have an uplifting conversation about local politics and become lifelong friends.')
+        }
+        else if (thirdAnswer === 'run') {
+            window.prompt ('Quickly, you run back to the caves entrance. Sheepish, you return home.')
+        }
 
+     }
+    else if (secondAnswer === 'Away'){
+    let thirdAnswer = window.prompt ('After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do? Draw it or Pick it')
+        if (thirdAnswer === 'Draw it') {
+            window.prompt ('You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.')
+        }
+        else if (thirdAnswer == 'Pick it'){
+            window.prompt ('You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.')
+        }
+    }
 }
